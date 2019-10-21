21ad89e675a6897072a21fb106698f41bdcf4080
wbragass <william.bragassa@yahoo.com>
2019-10-21 11:11:49 -0600

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-keys = {"office_url": "https://admin.microsoft.com/AdminPortal/Home#/users",
-        "email":"william.bragassa@apgdata.com",
-        "phrase":"()12qazWSX",
-        "user_name":"test user1"
-        }
